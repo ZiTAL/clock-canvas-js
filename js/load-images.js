84c65bf3b2327a9cdd914369280c414301379ac3
zital <zitalman@gmail.com>
2016-04-11 22:47:06 +0200

    :)

---
+++ js/load-images.js
@@ -0,0 +1,65 @@
+(function()
+{
+	'use strict';
+
+	var loadImages = function(arr, callback)
+	{
+		var count = 0;
+		var images = [];
+
+		for (var i in arr)
+		{
+			if (arr[i] !== '')
+			{
+				images[i] = new Image();
+				images[i].onload = function()
+				{
+					log(this.src + " image loaded");
+					count++;
+				};
+				images[i].onerror = function()
+				{
+					this.setAttribute('data-error', 'true');
+					log("error loading " + this.src + " image");
+					count++;
+				};
+				images[i].src = arr[i];
+			}
+			else
+			{
+				log("no image");
+				count++;
+			}
+		}
+
+		var loop = function()
+		{
+			if (count === arr.length)
+			{
+				var tmp = [];
+				for (var i in images)
+				{
+					if (images[i].hasAttribute('data-error') === false)
+						tmp.push(images[i]);
+				}
+				callback(tmp);
+			}
+			else
+			{
+				window.setTimeout(function()
+				{
+					loop();
+				}, 0);
+			}
+		};
+		
+		var log = function(str)
+		{
+			if(typeof window['console']!='undefined')
+				console.log(str);
+		}
+
+		loop();
+	};
+	window['loadImages'] = loadImages;
+})();
